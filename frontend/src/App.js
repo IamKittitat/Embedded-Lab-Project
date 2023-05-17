cba29a1f25379c8c7e4e662ce0f9dca2957807ee
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-05-17 10:31:06 +0700

    feat: create route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
-import About from "./pages/About";
-import Home from "./pages/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home/>,
+    path: '/',
+    element: <Home />,
   },
   {
-    path: "/about",
-    element: <About/>,
-  }
-
+    path: '/about',
+    element: <About />,
+  },
 ]);
 
 function App() {
