f7f79e5995d838fcb35265a2ac86b5b32ef4c984
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-05-17 09:33:04 +0700

    feat: Status Enum

---
+++ frontend/src/util/Status.js
@@ -0,0 +1,7 @@
+export const STATUS = {
+    PERFECT: 'Perfect',
+    WARNING: 'Warning',
+    PROCESS: 'Processing',
+  };
+  
+  export const STATUS_ENUM = [STATUS.PERFECT, STATUS.WARNING, STATUS.PROCESS];
