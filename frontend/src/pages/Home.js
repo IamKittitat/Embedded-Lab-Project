bd1a93833f9ecd68f0a9567fd07684994320aa2c
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-05-17 10:02:47 +0700

    feat: Home Page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/NavBar';
+import SideBar from '../components/SideBar';
+import StatusContainer from '../components/StatusContainer';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <SideBar/>
+            <StatusContainer/>
+      
+        </>
+    )
+}
+
+export default Home;
