3e98aebc3c64b8fcb6daaea6bdabc97e09d0f1e2
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-05-17 10:50:48 +0700

    feat: Colors

---
+++ frontend/src/components/Colors.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  darkbrown: '#614124',
+  lightgreen: '#F7F3CF',
+  lightgreen2: '#EBDDAD',
+  green: '#9FC088',
+  green2: '#9FC088', // Use in about goal
+  darkgreen: '#437421',
+  lightred: '#E8977D',
+  lightred2: '#EBC3AD',
+  red: '#D87B72',
+  darkred: '#CC4B4B',
+  lightblue: '#B9CFE4',
+  lightblue2: '#ADCDEB',
+  blue: '#7AACDA',
+  darkblue: '#4B8ECC',
+};
