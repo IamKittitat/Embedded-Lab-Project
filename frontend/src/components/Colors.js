f15b83b724145ca2bbe2dab80ca5aa02651feac2
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-05-17 13:52:57 +0700

    feat: update color

--- frontend/src/components/Colors.js
+++ frontend/src/components/Colors.js
@@ -2,11 +2,13 @@ export const COLORS = {
   darkbrown: '#614124',
   lightgreen: '#F7F3CF',
   lightgreen2: '#EBDDAD',
+  lightgreen3: '#F7E8CF',
   green: '#9FC088',
   green2: '#9FC088', // Use in about goal
   darkgreen: '#437421',
   lightred: '#E8977D',
   lightred2: '#EBC3AD',
+  lightred3: '#CC704B', // Use in Member Container
   red: '#D87B72',
   darkred: '#CC4B4B',
   lightblue: '#B9CFE4',
