eb357f8d0008c50cc5c9200cb0c9391257cd467f
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2023-05-23 10:30:29 +0700

    feat: style SideDetail

--- frontend/src/components/SideDetail.js
+++ frontend/src/components/SideDetail.js
@@ -5,7 +5,8 @@ import { COLORS } from './Colors';
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-around;
   align-items: center;
   background-color: ${(props) =>
     props.status === STATUS.PERFECT
@@ -13,26 +14,58 @@ const Container = styled.div`
       : props.status === STATUS.PROCESS
       ? COLORS.darkblue
       : COLORS.darkred};
-
-  color: ${COLORS.white};
+  border: 3px solid black;
   width: 305px;
   height: 40%;
   border-radius: 30px;
+`;
+
+const Text = styled.p`
+  color: ${COLORS.white};
   font-size: 40px;
   font-weight: 900;
   text-align: center;
-  border: 3px solid black;
+`;
+
+const FillWater = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 67px;
+  width: 245px;
+  border: 3px solid #000000;
+  border-radius: 100px;
+  background-color: #409bce;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: ${COLORS.white};
+  cursor: pointer;
 `;
 
 const SideDetail = ({ status }) => {
+  const waterHandler = () => {
+    alert('Watering Plant');
+  };
   return (
     <>
       {status === STATUS.PERFECT ? (
-        <Container status={status}>Everything is Perfect!</Container>
+        <Container status={status}>
+          <Text>Everything is Perfect!</Text>
+        </Container>
       ) : status === STATUS.WARNING ? (
-        <Container status={status}>WARNING</Container>
+        <Container status={status}>
+          <Text>WARNING!!</Text>
+          <FillWater onClick={waterHandler}>Watering Plant</FillWater>
+        </Container>
       ) : (
-        <Container status={status}>PROCESS</Container>
+        <Container status={status}>
+          <Text>
+            PROCESSING <br /> ...
+          </Text>
+        </Container>
       )}
     </>
   );
